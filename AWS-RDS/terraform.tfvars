15f5f1f13ed1dbda87758d00c00568d46c945417
rahulmajhi017 <debasisaj395@gmail.com>
2026-06-11 16:55:49 +0530

    Add files via upload

---
+++ AWS-RDS/terraform.tfvars
@@ -0,0 +1,26 @@
+# Valus For Value
+db_instance_class = "db.t3.micro"
+
+db_name = "mydatabase"
+
+db_engine = "postgres"
+
+db_engine_version = "15.7"
+
+db_engine_pggroup = "15"
+
+db_username = "Rahul-DB"
+
+db_password = "Root123456"
+
+allocated_storage = 20
+
+db_backup_retention_period = 7 
+
+db_backup_window = "03:00-04:00"
+
+db_maintenance_window = "sun:05:00-sun:06:00"
+
+region = "ap-south-1"
+
+ports = "22"
---
+++ AWS-RDS/variable.tf
@@ -0,0 +1,64 @@
+# Variable
+variable "region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+}
+
+variable "db_instance_class" {
+  description = "The instance type for the RDS instance"
+  type        = string
+}
+
+variable "db_name" {
+  description = "The name of the database to be created"
+  type        = string
+}
+
+variable "db_engine" {
+  description = "The database engine to use"
+  type        = string
+}
+
+variable "db_engine_version" {
+  description = "The version of the database engine"
+  type        = string
+}
+
+variable "db_engine_pggroup" {
+  description = "The version of the parameter group"
+  type        = string
+}
+
+variable "db_username" {
+  description = "The master username for the RDS instance"
+  type        = string
+}
+
+variable "db_password" {
+  description = "The master password for the RDS instance"
+  type        = string
+}
+
+variable "allocated_storage" {
+  description = "The allocated storage in GB"
+  type        = number
+}
+
+variable "db_backup_retention_period" {
+  description = "The number of days to retain backups"
+  type        = number
+}
+
+variable "db_backup_window" {
+  description = "The daily time range during which automated backups are created"
+  type        = string
+}
+
+variable "db_maintenance_window" {
+  description = "The weekly time range during which system maintenance can occur"
+  type        = string
+}
+
+variable "ports" {
+  type = string
+}
